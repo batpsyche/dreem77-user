421ece1245948e5ad910d56525d59721be90d247
DC_NOTEBOOK_WSL <tadka.harish54@gmail.com>
2024-03-20 16:31:33 +0530

    setup : work in progress setup for user panel (listed below)
    - footer year dates to current FY
    - image display on main banner homepage
    - redirecting after login to '/home/' instead of '/in-play'
    - showing cricket options and casino button on the homepage

--- src/app/View/Pages/login/login.component.ts
+++ src/app/View/Pages/login/login.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit, Input, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { LoginserviceService } from '../../../Service/User/loginservice.service';
-import { AuthService } from '../../../Service/auth.service';
-import { Globals } from '../../../Model/global';
+import { Component, OnInit, Input, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { LoginserviceService } from "../../../Service/User/loginservice.service";
+import { AuthService } from "../../../Service/auth.service";
+import { Globals } from "../../../Model/global";
 
 declare var $: any;
 
-
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
-
 export class LoginComponent implements OnInit {
   public image: string;
   public terms: Promise<boolean>;
@@ -33,86 +31,95 @@ export class LoginComponent implements OnInit {
   footerLogo;
   mobile;
   super_admin_commission_type;
-  Flogo = 'assets/loginimages/Flogo.png'
-  constructor(private http: HttpClient, private router: Router, private loginservice: LoginserviceService, private auth: AuthService, public global: Globals) {
+  Flogo = "assets/loginimages/Flogo.png";
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private loginservice: LoginserviceService,
+    private auth: AuthService,
+    public global: Globals
+  ) {
     this.router.errorHandler = (error: any) => {
-      this.router.navigate(['NotFound']); // or redirect to default route
-    }
+      this.router.navigate(["NotFound"]); // or redirect to default route
+    };
   }
   private message: any;
   is_show_apk;
   ngOnInit() {
-    this.footerLogo = this.global.Url + 'uploads/footer.png';
+    this.footerLogo = this.global.Url + "uploads/footer.png";
 
-    this.logo = this.global.Url + 'uploads/logo.png';
+    this.logo = this.global.Url + "uploads/logo.png";
     // this.bg_logo = this.global.Url + 'uploads/bg_logo.png';
     // this.bg_logo = this.global.Url + 'uploads/bg_logo.png' || 'assets/loginimages/login-bg.jpg';
-   // this.bg_logo = 'assets/loginimages/login-bg.jpg';
-    $.getScript('/assets/js/captcha.js');
-    this.g_captcha = localStorage.getItem('v3token');
+    // this.bg_logo = 'assets/loginimages/login-bg.jpg';
+    $.getScript("/assets/js/captcha.js");
+    this.g_captcha = localStorage.getItem("v3token");
     this.checkMaintenanceSetting();
-    this.apkUrl = this.global.Url + 'uploads/app.apk'
-    this.social_image = this.global.Url + 'uploads/social_logo.png';
+    this.apkUrl = this.global.Url + "uploads/app.apk";
+    this.social_image = this.global.Url + "uploads/social_logo.png";
     this.globalConstant();
-
   }
 
   checkMaintenanceSetting() {
-    this.loginservice.checkMaintenanceSetting().subscribe(response => {
-      if (response.code == '1407')
-        this.router.navigate(['/UnderMaintinance']);
-      else
-        this.router.navigate(['/login']);
-
-    }, error => {
-    });
+    this.loginservice.checkMaintenanceSetting().subscribe(
+      (response) => {
+        if (response.code == "1407")
+          this.router.navigate(["/UnderMaintinance"]);
+        else this.router.navigate(["/login"]);
+      },
+      (error) => {}
+    );
   }
 
   globalConstant() {
-    this.loginservice.globalConstant().subscribe(response => {
-
-      this.is_show_apk = response.data.ApkData.is_active;
-      this.super_admin_commission_type = response.data.super_admin_commission_type;
-      this.mobile = response.data.phone_no;
-      this.phone_no = response.data.country_code + ' ' + response.data.phone_no;
-      this.global.super_admin_commission_type = this.super_admin_commission_type;
-      this.global.PdcData = response.data;
-    }, error => {
-    });
+    this.loginservice.globalConstant().subscribe(
+      (response) => {
+        this.is_show_apk = response.data.ApkData.is_active;
+        this.super_admin_commission_type =
+          response.data.super_admin_commission_type;
+        this.mobile = response.data.phone_no;
+        this.phone_no =
+          response.data.country_code + " " + response.data.phone_no;
+        this.global.super_admin_commission_type =
+          this.super_admin_commission_type;
+        this.global.PdcData = response.data;
+      },
+      (error) => {}
+    );
   }
 
-
   onSubmit() {
-    debugger
-    this.g_captcha = localStorage.getItem('v3token');
+    debugger;
+    this.g_captcha = localStorage.getItem("v3token");
     this.userData = {
       user_name: this.user_name,
       password: this.password,
-      g_captcha: this.g_captcha
-    }
-    this.loginservice.submitUser(this.userData).subscribe((result) => {
-      if (!result.error) {
-        this.global.setUser(this.terms);
-        this.global.token = result.data.token;
-        this.auth.sendToken(result.data.token)
-        localStorage.setItem('UserLoginData', JSON.stringify(result.data));
-        localStorage.setItem('TokenId', JSON.stringify(result.data.token));
-        // this.router.navigate(['/dashboard/home']);
-        this.router.navigate(['/dashboard/in-play']); // New Home 
-
-      } else {
-
-        setTimeout(function () { $.getScript("/assets/js/captcha.js"); }, 1);
-        if (result.type != 3) {
-          this.showGreeting = true;
-          this.message = result.message;
+      g_captcha: this.g_captcha,
+    };
+    this.loginservice.submitUser(this.userData).subscribe(
+      (result) => {
+        if (!result.error) {
+          this.global.setUser(this.terms);
+          this.global.token = result.data.token;
+          this.auth.sendToken(result.data.token);
+          localStorage.setItem("UserLoginData", JSON.stringify(result.data));
+          localStorage.setItem("TokenId", JSON.stringify(result.data.token));
+          this.router.navigate(["/dashboard/home"]);
+          // this.router.navigate(['/dashboard/in-play']); // New Home
         } else {
-          this.showGreeting = true;
-          this.message = result.message;
+          setTimeout(function () {
+            $.getScript("/assets/js/captcha.js");
+          }, 1);
+          if (result.type != 3) {
+            this.showGreeting = true;
+            this.message = result.message;
+          } else {
+            this.showGreeting = true;
+            this.message = result.message;
+          }
         }
-      }
-    }, (err) => {
-
-    });
+      },
+      (err) => {}
+    );
   }
 }
--- src/app/View/layout/dashboard/dashboard.routing.module.ts
+++ src/app/View/layout/dashboard/dashboard.routing.module.ts
@@ -1,84 +1,91 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-    { path: '', redirectTo: 'in-play', pathMatch: 'full' },
-    {
-        path: 'in-play',
-        loadChildren: 'src/app/View/Pages/in-play/in-play.module#InPlayModule'
-    },
-    {
-        path: 'in-play-2',
-        loadChildren: 'src/app/View/Pages/in-play2/in-play.module#InPlayModule'
-    },
-    {
-        path: 'home', loadChildren: 'src/app/View/Pages/home/home.module#HomeModule',
-    },
-    {
-        path: 'UserMatchOdds',
-        loadChildren: 'src/app/View/Pages/user-match-odds/user-match-odds.module#UserMatchOddsModule'
-    },
-    {
-        path: 'Favourite',
-        loadChildren: 'src/app/View/Pages/favourite/favourite.module#FavouriteModule'
-    },
-    {
-        path: 'virtual-games',
-        loadChildren: 'src/app/View/Pages/virtual-games/virtual-games.module#VirtualGamesModule'
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "in-play",
+    loadChildren: "src/app/View/Pages/in-play2/in-play.module#InPlayModule",
+  },
+  {
+    path: "in-play-2",
+    loadChildren: "src/app/View/Pages/in-play2/in-play.module#InPlayModule",
+  },
+  {
+    path: "home",
+    loadChildren: "src/app/View/Pages/home/home.module#HomeModule",
+  },
+  {
+    path: "UserMatchOdds",
+    loadChildren:
+      "src/app/View/Pages/user-match-odds/user-match-odds.module#UserMatchOddsModule",
+  },
+  {
+    path: "Favourite",
+    loadChildren:
+      "src/app/View/Pages/favourite/favourite.module#FavouriteModule",
+  },
+  {
+    path: "virtual-games",
+    loadChildren:
+      "src/app/View/Pages/virtual-games/virtual-games.module#VirtualGamesModule",
+  },
+  {
+    path: "brino-fantasy",
+    loadChildren:
+      "src/app/View/Pages/brino-fantasy/brino-fantasy.module#BrinoFantasyModule",
+  },
 
-    }, {
-        path: 'brino-fantasy',
-        loadChildren: 'src/app/View/Pages/brino-fantasy/brino-fantasy.module#BrinoFantasyModule'
-    },
-
-    {
-        path: 'super-spade-games',
-        loadChildren: 'src/app/View/Pages/super-spade-games/super-spade-games.module#SuperSpadeGamesModule'
-
-    },
-    {
-        path: 'gamepool',
-        loadChildren: 'src/app/View/Pages/gamepool/gamepool.module#GamepoolModule'
-
-    },
-    // {
-    //     path: 'betsoft',
-    //     loadChildren: 'src/app/View/Pages/mega-win/mega-win.module#MegaWinModule'
-    // },
-    {
-        path: 'ezugi',
-        loadChildren: 'src/app/View/Pages/ezugi-games/ezugi-games.module#EzugiGamesModule'
-    },
-    // {
-    //     path: 'sa-games',
-    //     loadChildren: 'src/app/View/Pages/sa-games/sa-games.module#SaGamesModule'
-    // }, 
-    {
-        path: 'qt-games',
-        loadChildren: 'src/app/View/Pages/qt-games/qt-games.module#QtGamesModule'
-    },
-    {
-        path: 'game',
-        loadChildren: 'src/app/View/Pages/selected-games/selected-games.module#SelectedGamesModule'
-    },
-    {
-        path: 'tvbet',
-        loadChildren: 'src/app/View/Pages/tv-bet-games/tv-bet-games.module#TvBetGamesModule'
-    },
-    {
-        path: 'supernowa',
-        loadChildren: 'src/app/View/Pages/supernowa/supernowa.module#SupernowaModule'
-    },
-    {
-        path: 'all-game',
-        loadChildren: 'src/app/View/Pages/allgameiframe/allgameiframe.module#AllgameiframeModule'
-    }
+  {
+    path: "super-spade-games",
+    loadChildren:
+      "src/app/View/Pages/super-spade-games/super-spade-games.module#SuperSpadeGamesModule",
+  },
+  {
+    path: "gamepool",
+    loadChildren: "src/app/View/Pages/gamepool/gamepool.module#GamepoolModule",
+  },
+  // {
+  //     path: 'betsoft',
+  //     loadChildren: 'src/app/View/Pages/mega-win/mega-win.module#MegaWinModule'
+  // },
+  {
+    path: "ezugi",
+    loadChildren:
+      "src/app/View/Pages/ezugi-games/ezugi-games.module#EzugiGamesModule",
+  },
+  // {
+  //     path: 'sa-games',
+  //     loadChildren: 'src/app/View/Pages/sa-games/sa-games.module#SaGamesModule'
+  // },
+  {
+    path: "qt-games",
+    loadChildren: "src/app/View/Pages/qt-games/qt-games.module#QtGamesModule",
+  },
+  {
+    path: "game",
+    loadChildren:
+      "src/app/View/Pages/selected-games/selected-games.module#SelectedGamesModule",
+  },
+  {
+    path: "tvbet",
+    loadChildren:
+      "src/app/View/Pages/tv-bet-games/tv-bet-games.module#TvBetGamesModule",
+  },
+  {
+    path: "supernowa",
+    loadChildren:
+      "src/app/View/Pages/supernowa/supernowa.module#SupernowaModule",
+  },
+  {
+    path: "all-game",
+    loadChildren:
+      "src/app/View/Pages/allgameiframe/allgameiframe.module#AllgameiframeModule",
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
