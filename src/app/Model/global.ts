d272546b65631362b6d25f37c35d2a4bb664b713
DC_NOTEBOOK_WSL <tadka.harish54@gmail.com>
2024-03-25 05:02:46 +0530

    feature : showing live data before login
    - Visitor Look feature
    - Changes made in match.service
    - reloading whole Angular application as soon as user clicks on any button

--- src/app/Model/global.ts
+++ src/app/Model/global.ts
@@ -1,35 +1,51 @@
-import {Injectable} from '@angular/core';
-import {HttpHeaders} from '@angular/common/http';
-import {environment} from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpHeaders } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class Globals {
-    Url;
-    super_admin_commission_type;
-    static Back = 'Back';
-    static Lay = 'Lay';
-    public loginUserData;
-    public oneClickSelectedBet;
-    public IsToggle;
-    public btnActive;
-    private reqHeader: HttpHeaders;
-    public userData: any;
-    public  token:any;
-    public  PdcData:any;
-    constructor() {
-        this.Url = environment.APIEndpoint;
-        this.loginUserData = JSON.parse(localStorage.getItem('UserLoginData'));
-        this.reqHeader = new HttpHeaders({
-            Authorization: localStorage.getItem('TokenId'),
-        });
-    }
-    setUser(userData: any) {
-        this.userData = userData;
-    }
+  Url;
+  super_admin_commission_type;
+  static Back = "Back";
+  static Lay = "Lay";
+  public loginUserData;
+  public oneClickSelectedBet;
+  public IsToggle;
+  public btnActive;
+  private reqHeader: HttpHeaders;
+  public userData: any;
+  public token: any;
+  public PdcData: any;
+  constructor() {
+    this.Url = environment.APIEndpoint;
+    this.loginUserData = JSON.parse(localStorage.getItem("UserLoginData"));
+    this.reqHeader = new HttpHeaders({
+      Authorization: localStorage.getItem("TokenId"),
+    });
+  }
+  setUser(userData: any) {
+    this.userData = userData;
+  }
 
-    getUser() {
-        return this.userData;
-    }
+  getUser() {
+    return this.userData;
+  }
 
+  getVisitor() {
+    return {
+      name: "testu",
+      user_name: "testu",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiaWQiOjU4OTEsInJvbGUiOiJBZG1pblRlc3QiLCJwYXJlbnRfaWQiOiIwLDAsMCwxIiwidXNlcl90eXBlX2lkIjo1fSwiaWF0IjoxNzEwNTkxMTcxfQ.5ZAKiB_JTpaFdpqKia7b4228fCqO4EOiCDTrgbwKJv8",
+      user_id: 5891,
+      user_type_id: 5,
+      is_change_password: "0",
+      is_signup_user: "0",
+      stack: "100,200,500,1000,2000",
+      one_click_stack: "50,200,300",
+      lastLoginTime: 1710587026,
+      terms_conditions:
+        "<ul><li>In valid bets will be deleted automatically or manually and there will be no claim available for such bets </li><li>In case  of any dispute , Admin has to right to take the final decision </li><li>In a cancelled/NO Result / Abondened  match all bets will be cancelled </li><li>If a  match got  cancelled due to rain, all completed fancy/session market will be settled </li><li>invalid  bets(bets with unfair rate) can be  deleted even after settling . There  will be no claim </li><li>There can be some technical issue in the software. Admin is not reponsible for such case. Play at your own risk </li><li>In case of a tied match, all completed session markets will be settled </li></ul>",
+    };
+  }
 }
-
--- src/app/Service/Match/match.service.ts
+++ src/app/Service/Match/match.service.ts
@@ -1,125 +1,215 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpHeaderResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Globals } from '../../Model/global';
-
-
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpHeaderResponse,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Globals } from "../../Model/global";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MatchService {
-  public loginUserData: string | any | { old_password: string; newpassword: string; Renewpassword: string; user_id: string; user_type: any };
+  public loginUserData:
+    | string
+    | any
+    | {
+        old_password: string;
+        newpassword: string;
+        Renewpassword: string;
+        user_id: string;
+        user_type: any;
+      };
   public user: any;
 
-  public UserLoginData = JSON.parse(localStorage.getItem('UserLoginData'));
+  public UserLoginData =
+    JSON.parse(localStorage.getItem("UserLoginData")) ||
+    this.globals.getVisitor();
   public token = this.UserLoginData.token;
   reqHeader = new HttpHeaders({
-    Authorization: 'Bearer ' + this.token,
-    'Content-Type': 'application/json'
-
+    Authorization: "Bearer " + this.token,
+    "Content-Type": "application/json",
   });
 
   constructor(private http: HttpClient, private globals: Globals) {
-
     this.loginUserData = this.globals.loginUserData;
-
   }
   Base_Url = this.globals.Url;
 
   GetSoprtName(param): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'getAllSports', param, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "getAllSports", param, {
+      headers: this.reqHeader,
+    });
   }
   getSeriesLst(SportID: any): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'getAllSeries', SportID, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "getAllSeries", SportID, {
+      headers: this.reqHeader,
+    });
   }
   getMatchname(): Observable<any> {
-    return this.http.get<any>('https://score.crakex.in:3290/tv_code');
+    return this.http.get<any>("https://score.crakex.in:3290/tv_code");
   }
 
   getUserFavouriteMatchLst(SportData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'homematches', SportData, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "homematches", SportData, {
+      headers: this.reqHeader,
+    });
   }
   getGamesPool(): Observable<any> {
-    return this.http.get<any>(this.Base_Url + 'letplay/gameList', { headers: this.reqHeader });
+    return this.http.get<any>(this.Base_Url + "letplay/gameList", {
+      headers: this.reqHeader,
+    });
   }
   gotoGameURL(params): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'letplay/getGameUrl', params, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "letplay/getGameUrl", params, {
+      headers: this.reqHeader,
+    });
   }
   deleteUserbet(betId, UsereID): Observable<any> {
-    return this.http.get<any>(this.Base_Url + '/Betentrycntr/deleteGetbetting/' + betId + '/' + UsereID, { headers: this.reqHeader });
+    return this.http.get<any>(
+      this.Base_Url + "/Betentrycntr/deleteGetbetting/" + betId + "/" + UsereID,
+      { headers: this.reqHeader }
+    );
   }
 
   setFavorite(matchdata): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'addMarketFavourite', matchdata, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "addMarketFavourite",
+      matchdata,
+      { headers: this.reqHeader }
+    );
   }
   setUnFavorite(matchdata): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'addMarketFavourite', matchdata, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "addMarketFavourite",
+      matchdata,
+      { headers: this.reqHeader }
+    );
   }
 
   getChipDatabyId(): Observable<any> {
-    return this.http.get<any>(this.Base_Url + 'getUserBalance', { headers: this.reqHeader });
+    return this.http.get<any>(this.Base_Url + "getUserBalance", {
+      headers: this.reqHeader,
+    });
   }
   matchDetails(params): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'matchDetails', params, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "matchDetails", params, {
+      headers: this.reqHeader,
+    });
   }
   getAllFavouriteMarket(): Observable<any> {
-    return this.http.get<any>(this.Base_Url + 'getAllFavouriteMarket', { headers: this.reqHeader });
+    return this.http.get<any>(this.Base_Url + "getAllFavouriteMarket", {
+      headers: this.reqHeader,
+    });
   }
   getUserMatchStack(): Observable<any> {
-    return this.http.get<any>(this.Base_Url + 'getUserMatchStack', { headers: this.reqHeader });
+    return this.http.get<any>(this.Base_Url + "getUserMatchStack", {
+      headers: this.reqHeader,
+    });
   }
   updateUserMatchStack(param): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'updateUserMatchStack', param, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "updateUserMatchStack", param, {
+      headers: this.reqHeader,
+    });
   }
   updateUserOneClickStack(param): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'updateUserOneClickStack', param, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "updateUserOneClickStack",
+      param,
+      { headers: this.reqHeader }
+    );
   }
   saveBet(params): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'bet/saveBet', params, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "bet/saveBet", params, {
+      headers: this.reqHeader,
+    });
   }
   saveFancyBet(params): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'bet/saveFancyBet', params, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "bet/saveFancyBet", params, {
+      headers: this.reqHeader,
+    });
   }
   getBetsByMatchId(params): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'bet/getBetsByMatchId', params, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "bet/getBetsByMatchId", params, {
+      headers: this.reqHeader,
+    });
   }
   getAllBets(): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'bet/getAllBets', '', { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "bet/getAllBets", "", {
+      headers: this.reqHeader,
+    });
   }
   GetScoreApi(matchId): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'scoreBoard', matchId, { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "scoreBoard", matchId, {
+      headers: this.reqHeader,
+    });
   }
   searchMatchList(reportData: any): Observable<any> {
-    return this.http.get<any>(this.Base_Url + 'searchMatches?search=' + reportData, { headers: this.reqHeader });
+    return this.http.get<any>(
+      this.Base_Url + "searchMatches?search=" + reportData,
+      { headers: this.reqHeader }
+    );
   }
   logout(): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'logout', '', { headers: this.reqHeader });
+    return this.http.post<any>(this.Base_Url + "logout", "", {
+      headers: this.reqHeader,
+    });
   }
+
   getAllActiveNotifications(param): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'notifications/getAllActiveNotifications', param, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "notifications/getAllActiveNotifications",
+      param,
+      { headers: this.reqHeader }
+    );
   }
 
   changePassword(loginUserData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'updatePassword', loginUserData, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "updatePassword",
+      loginUserData,
+      { headers: this.reqHeader }
+    );
   }
   teenPattiMarketResult(loginUserData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'result/teenPattiMarketResult', loginUserData, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "result/teenPattiMarketResult",
+      loginUserData,
+      { headers: this.reqHeader }
+    );
   }
   marketResultListByMatchId(loginUserData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'result/marketResultListByMatchId', loginUserData, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "result/marketResultListByMatchId",
+      loginUserData,
+      { headers: this.reqHeader }
+    );
   }
   marketResultListByMatchIdWithOutPagination(loginUserData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'result/marketResultListByMatchIdWithOutPagination', loginUserData, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "result/marketResultListByMatchIdWithOutPagination",
+      loginUserData,
+      { headers: this.reqHeader }
+    );
   }
   getLiveSportMatchList(loginUserData): Observable<any> {
-    return this.http.post<any>(this.Base_Url + 'matches/getLiveSportMatchList', loginUserData, { headers: this.reqHeader });
+    return this.http.post<any>(
+      this.Base_Url + "matches/getLiveSportMatchList",
+      loginUserData,
+      { headers: this.reqHeader }
+    );
   }
   getCups() {
-    return this.http.post(this.Base_Url + 'matches/getCups', {}, { headers: this.reqHeader });
+    return this.http.post(
+      this.Base_Url + "matches/getCups",
+      {},
+      { headers: this.reqHeader }
+    );
   }
   getTVUrls() {
-    return this.http.get(this.Base_Url + 'gettvurl', { headers: this.reqHeader });
+    return this.http.get(this.Base_Url + "gettvurl", {
+      headers: this.reqHeader,
+    });
   }
-
 }
